e8c078dfabad38d5c99c2c085179b755aaf91bbd
fromSoundToDev <jeunejuste@gmail.com>
2025-12-18 20:18:22 +0000

    function and list

--- beginer/data_type.ts
+++ beginer/data_type.ts
@@ -1,1 +1,14 @@
-const addition  = (x:number , y : number )=> x+y ;
+const addition = (x: number, y: number): number => {
+    return x + y;
+};
+
+console.log(addition(5, 10)); // Résultat : 15
+
+// function and list
+const calculerMoyenne = (liste: number[]): number => {
+    const moyenne : number = liste.reduce((a,b)=> a+b,0) / liste.length;
+    return moyenne;
+};
+
+const mesNotes = [15, 12, 18, 10];
+console.log(calculerMoyenne(mesNotes));
